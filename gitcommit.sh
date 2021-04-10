9e6d3009a1a90fcd5445031d9d0e9d02dd2afc20
karansingh01 <karans@stud.ntnu.nosgit config --global user.email karanss@stud.ntnu.no git config --global user.email karanss@stud.ntnu.no>
2021-04-10 20:36:20 +0200

    testing file

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Branch name: $1"
+echo "Commit message: $1"
+echo "Branch name: $2"
 /usr/bin/git add .
 /usr/bin/git commit -m "$1"
-/usr/bin/git push origin master
+/usr/bin/git push origin $2
